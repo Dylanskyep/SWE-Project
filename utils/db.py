88a9e820f7dcc92c12628294932c36e6db39856c
Michael McCamy <michael.mccamy@ufl.edu>
2025-10-22 14:35:17 -0400

    Add event functions to events.py

--- utils/db.py
+++ utils/db.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase_config.json")  # replace with your new private key JSON
+cred = credentials.Certificate("firebase_config.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
